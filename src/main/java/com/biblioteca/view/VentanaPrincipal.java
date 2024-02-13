a9d6e93661f9456a68bb281c81f7a18e613e6bd1
Sayul <sayulramirez@gmail.com>
2024-02-13 14:20:10 -0600

    Agregando algunos colores y eventos a los botones del menú

--- src/main/java/com/biblioteca/view/VentanaPrincipal.java
+++ src/main/java/com/biblioteca/view/VentanaPrincipal.java
@@ -24,9 +24,11 @@ public class VentanaPrincipal extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         getContentPane().setLayout(new AbsoluteLayout());
+        getContentPane().setBackground(new Color(179, 8, 56));
 
         JLabel labelTitulo = new JLabel();
-        labelTitulo.setFont(new Font("Segoe UI", Font.BOLD, 26)); // NOI18N
+        labelTitulo.setFont(new Font("Segoe UI", Font.BOLD, 26));// NOI18N
+        labelTitulo.setForeground(new Color(250, 250, 250));
         labelTitulo.setHorizontalAlignment(SwingConstants.CENTER);
         labelTitulo.setText("Servicio  de prestamos bibliotecarios");
         getContentPane().add(labelTitulo, new AbsoluteConstraints(0, 0, 890, 40));
@@ -35,112 +37,186 @@ public class VentanaPrincipal extends JFrame {
         panelContenedor.setLayout(new AbsoluteLayout());
 
         JPanel panelMenu = new JPanel();
-        panelMenu.setBackground(new Color(231, 244, 237));
+        panelMenu.setBackground(new Color(224, 62, 82));
         panelMenu.setLayout(new AbsoluteLayout());
 
         JPanel panelAutenti = new JPanel();
-        panelAutenti.setBackground(new Color(231, 244, 237));
-        panelAutenti.setCursor(new Cursor(java.awt.Cursor.HAND_CURSOR));
+        JLabel labelAutenti = new JLabel();
+        panelAutenti.setBackground(new Color(113, 197, 232));
+        panelAutenti.setCursor(new Cursor(Cursor.HAND_CURSOR));
         panelAutenti.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
                 panelAutentiMouseClicked();
             }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                panelAutenti.setBackground(new Color(0, 165, 181));
+                labelAutenti.setFont(new Font("Segoe UI", Font.PLAIN, 19));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                panelAutenti.setBackground(new Color(113, 197, 232));
+                labelAutenti.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+            }
         });
         panelAutenti.setLayout(new AbsoluteLayout());
 
-        JLabel labelAutenti = new JLabel();
         labelAutenti.setFont(new Font("Segoe UI", Font.PLAIN, 17)); // NOI18N
         labelAutenti.setHorizontalAlignment(SwingConstants.CENTER);
-        labelAutenti.setText("Autenticación de alumno");
+        labelAutenti.setForeground(new Color(179, 8, 56));
+        labelAutenti.setText("Autenticar alumno");
         panelAutenti.add(labelAutenti, new AbsoluteConstraints(0, 0, 200, 30));
 
         panelMenu.add(panelAutenti, new AbsoluteConstraints(0, 140, 200, 30));
 
         JPanel panelEstado = new JPanel();
-        panelEstado.setBackground(new Color(231, 244, 237));
+        JLabel labelEstado = new JLabel();
+        panelEstado.setBackground(new Color(113, 197, 232));
         panelEstado.setCursor(new Cursor(Cursor.HAND_CURSOR));
         panelEstado.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
                 panelEstadoMouseClicked();
             }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                panelEstado.setBackground(new Color(0, 165, 181));
+                labelEstado.setFont(new Font("Segoe UI", Font.PLAIN, 19));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                panelEstado.setBackground(new Color(113, 197, 232));
+                labelEstado.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+            }
         });
         panelEstado.setLayout(new AbsoluteLayout());
 
-        JLabel labelEstado = new JLabel();
         labelEstado.setFont(new Font("Segoe UI", Font.PLAIN, 17)); // NOI18N
         labelEstado.setHorizontalAlignment(SwingConstants.CENTER);
+        labelEstado.setForeground(new Color(179, 8, 56));
         labelEstado.setText("Estado");
         panelEstado.add(labelEstado, new AbsoluteConstraints(0, 0, 200, 30));
 
         panelMenu.add(panelEstado, new AbsoluteConstraints(0, 170, 200, 30));
 
         JPanel panelConLibro = new JPanel();
-        panelConLibro.setBackground(new Color(231, 244, 237));
+        JLabel labelConLibro = new JLabel();
+        panelConLibro.setBackground(new Color(113, 197, 232));
         panelConLibro.setCursor(new Cursor(Cursor.HAND_CURSOR));
         panelConLibro.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
                 panelConLibroMouseClicked();
             }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                panelConLibro.setBackground(new Color(0, 165, 181));
+                labelConLibro.setFont(new Font("Segoe UI", Font.PLAIN, 19));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                panelConLibro.setBackground(new Color(113, 197, 232));
+                labelConLibro.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+            }
         });
         panelConLibro.setLayout(new AbsoluteLayout());
 
-        JLabel labelConLibro = new JLabel();
         labelConLibro.setFont(new Font("Segoe UI", Font.PLAIN, 17)); // NOI18N
         labelConLibro.setHorizontalAlignment(SwingConstants.CENTER);
-        labelConLibro.setText("Consulta de libros");
+        labelConLibro.setForeground(new Color(179, 8, 56));
+        labelConLibro.setText("Consulta libro");
         panelConLibro.add(labelConLibro, new AbsoluteConstraints(0, 0, 200, 30));
 
         panelMenu.add(panelConLibro, new AbsoluteConstraints(0, 200, 200, 30));
 
         JPanel panelConPrest = new JPanel();
-        panelConPrest.setBackground(new Color(231, 244, 237));
+        JLabel labelConPrest = new JLabel();
+        panelConPrest.setBackground(new Color(113, 197, 232));
         panelConPrest.setCursor(new Cursor(java.awt.Cursor.HAND_CURSOR));
         panelConPrest.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
                 panelConPrestMouseClicked();
             }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                panelConPrest.setBackground(new Color(0, 165, 181));
+                labelConPrest.setFont(new Font("Segoe UI", Font.PLAIN, 19));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                panelConPrest.setBackground(new Color(113, 197, 232));
+                labelConPrest.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+            }
         });
         panelConPrest.setLayout(new AbsoluteLayout());
 
-        JLabel labelConPrest = new JLabel();
         labelConPrest.setFont(new Font("Segoe UI", Font.PLAIN, 17)); // NOI18N
         labelConPrest.setHorizontalAlignment(SwingConstants.CENTER);
-        labelConPrest.setText("Consulta de prestamos");
+        labelConPrest.setForeground(new Color(179, 8, 56));
+        labelConPrest.setText("Consulta prestamo");
         panelConPrest.add(labelConPrest, new AbsoluteConstraints(0, 0, 200, 30));
 
         panelMenu.add(panelConPrest, new AbsoluteConstraints(0, 230, 200, 30));
 
         JPanel panelPrestamo = new JPanel();
-        panelPrestamo.setBackground(new Color(231, 244, 237));
+        JLabel labelPrestano = new JLabel();
+        panelPrestamo.setBackground(new Color(113, 197, 232));
         panelPrestamo.setCursor(new Cursor(java.awt.Cursor.HAND_CURSOR));
         panelPrestamo.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
                 panelPrestamoMouseClicked();
             }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                panelPrestamo.setBackground(new Color(0, 165, 181));
+                labelPrestano.setFont(new Font("Segoe UI", Font.PLAIN, 19));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                panelPrestamo.setBackground(new Color(113, 197, 232));
+                labelPrestano.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+            }
         });
         panelPrestamo.setLayout(new AbsoluteLayout());
 
-        JLabel labelPrestano = new JLabel();
         labelPrestano.setFont(new Font("Segoe UI", Font.PLAIN, 17)); // NOI18N
         labelPrestano.setHorizontalAlignment(SwingConstants.CENTER);
+        labelPrestano.setForeground(new Color(179, 8, 56));
         labelPrestano.setText("Prestamos");
         panelPrestamo.add(labelPrestano, new AbsoluteConstraints(0, 0, 200, 30));
 
         panelMenu.add(panelPrestamo, new AbsoluteConstraints(0, 260, 200, 30));
 
         JPanel panelDevolucion = new JPanel();
-        panelDevolucion.setBackground(new Color(231, 244, 237));
+        JLabel labelDevolucion = new JLabel();
+        panelDevolucion.setBackground(new Color(113, 197, 232));
         panelDevolucion.setCursor(new Cursor(java.awt.Cursor.HAND_CURSOR));
         panelDevolucion.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
                 panelDevolucionMouseClicked();
             }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                panelDevolucion.setBackground(new Color(0, 165, 181));
+                labelDevolucion.setFont(new Font("Segoe UI", Font.PLAIN, 19));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                panelDevolucion.setBackground(new Color(113, 197, 232));
+                labelDevolucion.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+            }
         });
         panelDevolucion.setLayout(new AbsoluteLayout());
 
-        JLabel labelDevolucion = new JLabel();
         labelDevolucion.setFont(new Font("Segoe UI", Font.PLAIN, 17)); // NOI18N
         labelDevolucion.setHorizontalAlignment(SwingConstants.CENTER);
+        labelDevolucion.setForeground(new Color(179, 8, 56));
         labelDevolucion.setText("Devoluciones");
         panelDevolucion.add(labelDevolucion, new AbsoluteConstraints(0, 0, 200, 30));
 
